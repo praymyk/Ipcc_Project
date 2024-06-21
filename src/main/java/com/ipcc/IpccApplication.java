2e3d8968ed5b28b1135900c49700f602bb039fef
YoonSeok Jeong <103018869+praymyk@users.noreply.github.com>
2024-06-21 20:16:45 +0900

    강제 커밋
    깃 베시로 안돼서 웹에서 시도

---
+++ src/main/java/com/ipcc/IpccApplication.java
@@ -0,0 +1,13 @@
+package com.ipcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpccApplication.class, args);
+	}
+
+}
