9bfeddf53ebccc8644604ce34172064477bcf029
praymyk <praymyk@gmail.com>
2024-07-05 18:54:15 +0900

    상세 메뉴 추가

--- src/main/java/com/ipcc/manager/controller/common/LoginController.java
+++ src/main/java/com/ipcc/manager/controller/common/LoginController.java
@@ -12,11 +12,17 @@ public class LoginController {
         // 로그인 페이지로 리다이렉트
         @PostMapping("/mlogin")
         public String redirectToLogin() {
-                log.info("잘드어오나?");
+
 
                 // 로그인 처리 작성
 
 
                 return "redirect:/manager/main";
+        }
+
+        // 로그인 성공 후 메인 페이지 이동
+        @GetMapping("/manager/main")
+        public String main() {
+                return "manager/main";
         }
 }
